b1c99418fa8fc0283c2e03b802e77e2b560475b4
h-murphy <hannah.murphy@wellesley.edu>
2015-12-02 10:15:02 -0500

    hannah
    hi

--- Map.java
+++ Map.java
@@ -2,5 +2,6 @@ public class Map{
   public static void main(String[] args){
     System.out.println("Hello Alyssa and Hannah!");
     System.out.println("new change");
+    System.out.println("This is Hannah");
   }
 }
