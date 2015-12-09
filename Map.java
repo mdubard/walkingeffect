92a708fa113ebdc5e131b6cb3999051ea181d4e4
mdubard <mary.dubard96@gmail.com>
2015-12-09 15:05:23 -0500

    GUI
    Working Submit button
    Map w/ key

--- Map.java
+++ Map.java
@@ -1,8 +1,4 @@
 public class Map{
-  public static void main(String[] args){
-    System.out.println("Hello Alyssa and Hannah!");
-    System.out.println("new change");
-    System.out.println("This is Hannah");
-    System.out.println("This is Alyssa");
+  public Map(){
   }
 }
--- Map.java~
+++ Map.java~
@@ -3,5 +3,6 @@ public class Map{
     System.out.println("Hello Alyssa and Hannah!");
     System.out.println("new change");
     System.out.println("This is Hannah");
+    System.out.println("This is Alyssa");
   }
 }
