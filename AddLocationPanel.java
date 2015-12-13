dc607ddb3c94c51c39c5830dac92aad4a4c687a0
h-murphy <hannah.murphy@wellesley.edu>
2015-12-13 12:17:54 -0500

    update text for addlocation

--- AddLocationPanel.java
+++ AddLocationPanel.java
@@ -21,90 +21,60 @@ public class AddLocationPanel extends JPanel{
     instanceMap = map;
     setLayout (new BorderLayout());
     
-    //set font to default as helvetica
-    Font headerFont = new Font("Helvetica", Font.PLAIN, 20);
-    Font customFont = new Font("Helvetica", Font.PLAIN, 15);
-    Font keyFont = new Font("Helvetica", Font.PLAIN, 13);
-    
-    try {
-      //create the font to use. Specify the size!
-      headerFont = Font.createFont(Font.TRUETYPE_FONT, new File("fontBold.ttf")).deriveFont(25f);
-      customFont = Font.createFont(Font.TRUETYPE_FONT, new File("font.ttf")).deriveFont(20f);
-      keyFont = Font.createFont(Font.TRUETYPE_FONT, new File("font.ttf")).deriveFont(15f);
-      GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-      //register the font
-      ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("font.ttf")));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    catch(FontFormatException e)
-    {
-      e.printStackTrace();
-    }
-    
     header = new JLabel("\nFill in the fields below to add a custom location to your map!", SwingConstants.CENTER);
-    header.setFont(headerFont);
+    header.setFont(new Font("Courier New", Font.PLAIN, 16));
     
     locs = instanceMap.getLocations();
     
     comboLocs = instanceMap.getLocationsCombo();
       //initialize combo boxes, using String array ratings for values
     nearbyLoc1Combo = new JComboBox(comboLocs);
-    nearbyLoc1Combo.setFont(keyFont);
     nearbyLoc2Combo = new JComboBox(comboLocs);
-    nearbyLoc1Combo.setFont(keyFont);
-
     
-    enterName = new JLabel("Name of New Location: ");
-    enterName.setFont(customFont);
-    JPanel locNamePanel = new JPanel();
+    enterName = new JLabel("Name of new Location");
+    enterName.setFont(new Font("Courier New", Font.PLAIN, 15));
     locName = new JTextField(20);
-    locNamePanel.add(locName);
     
     //initializes labels for combo boxes
     nearbyLoc1 = new JLabel("Nearby Location 1: ");
-    nearbyLoc1.setFont(customFont);
+    nearbyLoc1.setFont(new Font("Courier New", Font.PLAIN, 15));
     nearbyLoc2 = new JLabel("Nearby Location 2: ");
-    nearbyLoc2.setFont(customFont);
+    nearbyLoc2.setFont(new Font("Courier New", Font.PLAIN, 15));
     
-    JPanel distField1Panel = new JPanel();
     dist1 = new JLabel("Distance to Location 1: ");
-    dist1.setFont(customFont);
     distField1 = new JTextField(10);
-    distField1Panel.add(distField1);
+    time1 = new JLabel("Time to Location 1: ");
+    timeField1 = new JTextField(10);
     hasHillsCheck1 = new JCheckBox("The Path to Location 1 has Hills");
-    hasHillsCheck1.setFont(keyFont);
     hasStairsCheck1 = new JCheckBox("The Path to Location 1 has Stairs");
-    hasStairsCheck1.setFont(keyFont);
     
-    JPanel distField2Panel = new JPanel();
     dist2 = new JLabel("Distance to Location 2: ");
-    dist2.setFont(customFont);
     distField2 = new JTextField(20);
-    distField2Panel.add(distField2);
+    time2 = new JLabel("Time to Location 2: ");
+    timeField2 = new JTextField(20);
     hasHillsCheck2 = new JCheckBox("The Path to Location 2 has Hills");
-    hasHillsCheck2.setFont(keyFont);
     hasStairsCheck2 = new JCheckBox("The Path to Location 2 has Stairs");
-    hasStairsCheck2.setFont(keyFont);
     
     //creates submit button
     add = new JButton("Add Location");
     add.setPreferredSize(new Dimension(40, 40));
     add.addActionListener(new AddButtonListener());
-    add.setFont(customFont);
+    add.setFont(new Font("Courier New", Font.PLAIN, 12));
     
     //Creates panel for navigation options
     navi = new JPanel();
     navi.setLayout(new BoxLayout(navi, BoxLayout.Y_AXIS));
     navi.add(Box.createRigidArea(new Dimension(0, 50)));
     navi.add(enterName);
-    navi.add(locNamePanel);
+    navi.add(locName);
     
     navi.add(nearbyLoc1);
     //navi.add(Box.createRigidArea(new Dimension(0, 5)));
     navi.add(nearbyLoc1Combo);
     navi.add(dist1);
-    navi.add(distField1Panel);
+    navi.add(distField1);
+    //navi.add(time1);
+    //navi.add(timeField1);
     navi.add(hasHillsCheck1);
     navi.add(hasStairsCheck1);
     
@@ -112,7 +82,7 @@ public class AddLocationPanel extends JPanel{
     navi.add(nearbyLoc2);
     navi.add(nearbyLoc2Combo);
     navi.add(dist2);
-    navi.add(distField2Panel);
+    navi.add(distField2);
     navi.add(hasHillsCheck2);
     navi.add(hasStairsCheck2);
     navi.add(add);
@@ -140,7 +110,6 @@ public class AddLocationPanel extends JPanel{
     }
     
     keyText = new JTextArea(12, 20);
-    keyText.setFont(keyFont);
     keyText.setMaximumSize(keyText.getPreferredSize());
     keyText.setEditable(false);
     keyText.append("Map Key: ");
@@ -153,15 +122,13 @@ public class AddLocationPanel extends JPanel{
     JScrollPane jp = new JScrollPane(keyText);
     jp.setMaximumSize(keyText.getPreferredSize());
     
-    availLocs = new JLabel("Already Added Locations: ");
-    availLocs.setFont(keyFont);
+    availLocs = new JLabel("Already Added Locations:");
     
     mapPic.add(jp);
     
     //Initializes footer
-    footer = new JLabel("", SwingConstants.CENTER);
-    footer.setFont(customFont);
-
+    footer = new JLabel("<Directions here>", SwingConstants.CENTER);
+    footer.setFont(new Font("Courier New", Font.PLAIN, 16));
     
     //adds elements to frame
     add(header, BorderLayout.NORTH);
@@ -175,7 +142,7 @@ public class AddLocationPanel extends JPanel{
     private String newNearbyLoc2;
     
     public void actionPerformed(ActionEvent event){
-      footer.setText("Yo");
+      //footer.setText("Yo");
       
       //save combo box values as a string, if no value was chosen, the default value is 1
       String newLocName = locName.getText();
@@ -189,12 +156,15 @@ public class AddLocationPanel extends JPanel{
       }
       footer.setText(s);
 */
+      boolean isFirstDest = false;
+      boolean isSecondDest = false;
       
       newNearbyLoc1 = nearbyLoc1Combo.getSelectedItem().toString();
-      if(newNearbyLoc1 != "No location selected."){
+      if(!newNearbyLoc1.equals("No location selected.")){
         try{
           //footer.setText("You have to enter at least one nearby location! try again");
         //newNearbyLoc1 = nearbyLoc1Combo.getSelectedItem().toString();
+          isFirstDest = true;
         double locDist1 = Double.parseDouble(distField1.getText());
         //double timeDist1 = Double.parseDouble(timeField1.getText());
         boolean hasHills1 = hasHillsCheck1.isSelected();
@@ -208,13 +178,14 @@ public class AddLocationPanel extends JPanel{
       }catch(NumberFormatException n){
         System.out.println("You must enter valid input");
       }
-      }else{
-         footer.setText("You have to enter at least one nearby location! try again");
-      }
+      
+      
       
       newNearbyLoc2 = nearbyLoc2Combo.getSelectedItem().toString();
-      if(newNearbyLoc2 != "No location selected."){
+      System.out.println(newNearbyLoc2.equals("No location selected."));
+      if(!newNearbyLoc2.equals("No location selected.")){
         try{
+          isSecondDest = true;
           //footer.setText("You have to enter at least one nearby location! try again");
           double locDist2 = Double.parseDouble(distField2.getText());
           //double timeDist1 = Double.parseDouble(timeField1.getText());
@@ -229,8 +200,12 @@ public class AddLocationPanel extends JPanel{
           System.out.println("You must enter valid input");
         }
       }
-      footer.setText("New Location \"" + l.toString() + "\" has been added to the map, along with paths that lead to " + newNearbyLoc1 + " and " + newNearbyLoc2 + ".");
-      
+      String footerText =  "New Location \"" + l.toString() + "\" has been added to the map,";
+       footerText += isSecondDest ? " along with paths that lead to " + newNearbyLoc1 + " and " + newNearbyLoc2 + "." : " along with a path that leads to " + newNearbyLoc1;
+      footer.setText(footerText);
+      }else{
+         footer.setText("You have to enter at least one nearby location! try again");
+      }
       locs = instanceMap.getLocations();
       keyText.setText("");
       keyText.append("Map Key: ");
