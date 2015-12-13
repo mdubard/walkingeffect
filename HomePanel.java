08338452e1c71d1b3f52394f87cefcbdf42847ef
h-murphy <hannah.murphy@wellesley.edu>
2015-12-13 15:26:55 -0500

    updating home panel

--- HomePanel.java
+++ HomePanel.java
@@ -23,12 +23,34 @@ public class HomePanel extends JPanel implements ComponentListener{
   private JPanel navi, map;
   private JComboBox orig, dest;
   private JButton submit;
-  private JTextArea keyText;
+  private static JTextArea keyText;
   private JTextPane directions;
   private String[] locs;
   private Map m;
   private JCheckBox stairs;
   private JCheckBox steep;
+  
+  public static void setKeyText(String[] locs){
+    keyText.setText("");
+    keyText.append("Map Key: ");
+    //keyText.setRows(1);
+    for(int i = 0; i < locs.length; i++){
+      keyText.append("\n");
+      keyText.append(locs[i]);
+    }
+    //keyText.setFont(keyFont);
+  }
+  
+  public static void setComboBoxes(String[] locs){
+    keyText.setText("");
+    keyText.append("Map Key: ");
+    //keyText.setRows(1);
+    for(int i = 0; i < locs.length; i++){
+      keyText.append("\n");
+      keyText.append(locs[i]);
+    }
+    //keyText.setFont(keyFont);
+  }
   
   public HomePanel(Map ma){
     //on load reload locs?
@@ -67,10 +89,10 @@ public class HomePanel extends JPanel implements ComponentListener{
     orig.setFont(keyFont);
     dest = new JComboBox(locs);
     dest.setFont(keyFont);
-    //stairs = new JCheckBox("Avoid Stairs");
-    //stairs.setFont(keyFont);
-    //steep = new JCheckBox("Avoid Hills");
-    //steep.setFont(keyFont);
+    stairs = new JCheckBox("Avoid Stairs");
+    stairs.setFont(keyFont);
+    steep = new JCheckBox("Avoid Hills");
+    steep.setFont(keyFont);
     
     //initializes labels for combo boxes
     origin = new JLabel("Origin: ");
@@ -93,8 +115,8 @@ public class HomePanel extends JPanel implements ComponentListener{
     navi.add(Box.createRigidArea(new Dimension(0, 50)));
     navi.add(destination);
     navi.add(dest);
-    //navi.add(stairs);
-    //navi.add(steep);
+    navi.add(stairs);
+    navi.add(steep);
     navi.add(Box.createRigidArea(new Dimension(0, 100)));
     navi.add(submit);
     navi.add(Box.createRigidArea (new Dimension (0, 100)));
@@ -122,7 +144,7 @@ public class HomePanel extends JPanel implements ComponentListener{
     
     keyText = new JTextArea(12, 20);
     keyText.setMaximumSize(keyText.getPreferredSize());
-    keyText.setEditable(false);
+    //keyText.setEditable(false);
     keyText.append("Map Key: ");
     //keyText.setRows(1);
     for(int i = 0; i < locs.length; i++){
@@ -176,20 +198,20 @@ public class HomePanel extends JPanel implements ComponentListener{
       //save combo box values as a string, if no value was chosen, the default value is 1
       String origString = orig.getSelectedItem().toString();
       String destString = dest.getSelectedItem().toString();
-      //String stairsString = "Stairs an option. ";
-      //String hills = "Hills an option. ";
+      String stairsString = "Stairs an option. ";
+      String hills = "Hills an option. ";
       boolean avoidStairs = false;
       boolean avoidHills = false;
-      /*if(stairs.isSelected()){
+      if(stairs.isSelected()){
         stairsString = "Stairs not an option. ";
         avoidStairs = true;
       }
       if(steep.isSelected()){
         hills = "Hills not an option. ";
         avoidHills = true;
-      }*/
+      }
       
-      directions.setText("Directions from " + origString + " to " + destString + ". " + "\n" + m.directionsString(m.findLocation(origString), m.findLocation(destString)));
+      directions.setText("Directions from " + origString + " to " + destString + ". " + stairsString + hills + "\n" + m.directionsString(m.findLocation(origString), m.findLocation(destString)));
     }
   }
 }
